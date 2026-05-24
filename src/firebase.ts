c6b0581968a4a4a95a50af849ad3abd798e7fb83
AMA593 <142753062+AMA593@users.noreply.github.com>
2026-05-24 20:28:38 +0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBJpl1Ur_c0xiZdJAJtTUIloTKcO_kufgo",
   authDomain: "hcb-admin.firebaseapp.com",
@@ -16,6 +13,7 @@ const firebaseConfig = {
   measurementId: "G-XC7RPKZQ03"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+// export const analytics = getAnalytics(app); // Requires VITE environment usually, commented out for now
